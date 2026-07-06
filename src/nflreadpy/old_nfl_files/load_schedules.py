13ab4c1ab1425864b09c5ef73c92fccae050e3e1
juliawoeste <75594788+juliawoeste@users.noreply.github.com>
2026-07-06 10:22:18 -0400

    small updates to run main.py

--- src/nflreadpy/old_nfl_files/load_schedules.py
+++ src/nflreadpy/old_nfl_files/load_schedules.py
@@ -3,7 +3,7 @@
 import polars as pl
 
 from .downloader import get_downloader
-from .utils_date import get_current_season
+from ..utils_date import get_current_season
 
 
 def load_schedules(seasons: int | list[int] | bool | None = True) -> pl.DataFrame:
